64edebd1d785efdaf96cefb5cb129e869db3f3da
Alastair Gould <alastairgould@gmail.com>
2018-04-17 22:01:31 +0100

    add setup stuff

--- Setup-Cluster.ps1
+++ Setup-Cluster.ps1
@@ -1,1 +1,4 @@
 
+.\traefik-on-service-fabric\Traefik\Scripts\Deploy-FabricApplication.ps1 -ApplicationPackagePath ".\traefik-on-service-fabric\Traefik\pkg\Debug"
+.\traefik-on-service-fabric\ExampleApplication\ExampleApplication\Scripts\Deploy-FabricApplication.ps1 -ApplicationPackagePath ".\ExampleApplication\ExampleApplication\pkg\Debug"
+.\traefik-on-service-fabric\ExampleApplication\ExampleApplication2Scripts\Deploy-FabricApplication.ps1 -ApplicationPackagePath ".\ExampleApplication2\ExampleApplication2\pkg\Debug"
